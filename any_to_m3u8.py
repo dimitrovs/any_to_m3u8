41d1d2dd6abac0dfbc2d889c65b4e0ea3ab9feb7
root <root@server1.(none)>
2015-06-19 00:43:48 -0400

    Added support for IPv6 and a number of improvements to the FFMPEG interface.

--- any_to_m3u8.py
+++ any_to_m3u8.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 import SimpleHTTPServer
 import SocketServer
+import socket
 import subprocess
-import os.path
+import shlex
+import os
 import time
 import pipes
+from BaseHTTPServer import HTTPServer
 
 # CONFIG PARAMETERS
 
-SERVER_URL = 'http://your_server.com:14703/' # Your server's public URL
-HOST = '0.0.0.0' # Listen on on all IPs
-PORT = 14703 # 
+SERVER_URL = 'http://your-url.com/' # Your server's public URL
+#HOST = '::' # Listen on all IPs (IPv6)
+HOST = '0.0.0.0' # Listen on all IPs (IPv4)
+PORT = 80 # 
 DEBUG = False
+
 # END CONFIG
 
+logfile = open('result.log','w')
 current_url = ''
+current_process = None
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_HEAD(self):
@@ -23,20 +30,34 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	self.send_response('200')
     def do_GET(self):
 	global current_url
+	global current_process
         if self.path[5:7] == ':/':
 		url = pipes.quote(self.path[1:])
 		if url != current_url:
-			subprocess.call("/usr/bin/killall -9 ffmpeg", shell=True)
-			subprocess.call("rm -rf *.ts", shell=True)
-			subprocess.call("rm -rf *.m3u8", shell=True)
-			if DEBUG:
-				subprocess.Popen("ffmpeg -i "+url+" -acodec copy -vcodec copy -bsf:v h264_mp4toannexb -flags -global_header -hls_wrap 15 -hls_flags delete_segments -hls_base_url "+SERVER_URL+" out.m3u8 2> result.log &",shell=True)
+			if current_process:
+				# print "New URL requested. FFMPEG process detected. Terminating FFMPEG."
+				try:
+					current_process.kill()
+					# print "Waiting for FFMPEG to terminate..."
+					current_process.wait()
+					# print "FFMPEG terminated. Cleaning up files."
+				except:
+					# print "Exception while terminating FFMPEG. Proceeeding." 
+					pass
+				current_process = None
 			else:
-				subprocess.Popen("ffmpeg -loglevel 0 -i "+url+" -acodec copy -vcodec copy -bsf:v h264_mp4toannexb -flags -global_header -hls_wrap 15 -hls_flags delete_segments -hls_base_url "+SERVER_URL+" out.m3u8 2> /dev/null &",shell=True)
+				# print "New URL requested. No FFMPEG process detected. Proceeeding."
+				pass
+			[os.remove(f) for f in os.listdir(".") if f.endswith(".ts") or f.endswith(".m3u8")]
+			if DEBUG:
+				popen_args = shlex.split("ffmpeg -i "+url+" -acodec copy -vcodec copy -bsf:v h264_mp4toannexb -flags -global_header -hls_time 5 -hls_wrap 15 -hls_flags delete_segments -hls_base_url "+SERVER_URL+" out.m3u8")
+			else:
+				popen_args = shlex.split("ffmpeg -v -8 -i "+url+" -acodec copy -vcodec copy -bsf:v h264_mp4toannexb -flags -global_header -hls_time 5 -hls_wrap 15 -hls_flags delete_segments -hls_base_url "+SERVER_URL+" out.m3u8")
+			current_process = subprocess.Popen(popen_args,stderr=subprocess.STDOUT,stdout=logfile)
 			attempt = 0
-			while not os.path.exists('out0.ts'):
-				time.sleep(2)
+			while not os.path.exists('out.m3u8') and current_process.poll() is None:
 				attempt += 1
+				time.sleep(2)
 				if attempt > 30:
 					break
 		current_url = url
@@ -47,7 +68,39 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         else:
 		return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
-Handler = MyRequestHandler
-server = SocketServer.TCPServer((HOST, PORT), Handler)
+class HTTPServerV6(HTTPServer):
+  address_family = socket.AF_INET6
 
-server.serve_forever()
+def is_valid_ipv4_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  # not a valid address
+        return False
+
+    return True
+
+def is_valid_ipv6_address(address):
+    try:
+        socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:  # not a valid address
+        return False
+    return True
+
+Handler = MyRequestHandler
+server = None
+
+if is_valid_ipv6_address(HOST):
+	server = HTTPServerV6((HOST, PORT), Handler)
+elif is_valid_ipv4_address(HOST):
+	server = SocketServer.TCPServer((HOST, PORT), Handler)
+else:
+	print "ERROR: The HOST configuration parameter is not a valid IPv4 or IPv6 address."
+
+if server:
+	server.serve_forever()
